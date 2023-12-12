ffe356ebeee9ca3123cf57da737f08587bc552e8
Leo <benemax2001@gmail.com>
2023-12-12 14:08:14 +0000

    Done. missing phone styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,162 @@
 import React, { useEffect, useState } from "react"
 function App() {
-  const [result, setResult] = useState(false);
+  const [showResult, setShowResult] = useState(false);
+  const [result, setResult] = useState();
   const [score, setScore] = useState(0);
   const [rules, setRules] = useState(false);
   const [selected, setSelectd] = useState();
+  const [housePick, setHousePick] = useState();
+  const options = ["scissors", "spock", "paper", 'lizard', 'rock'];
+
+  function checkResult() {
+    if (!housePick) return;
+    if (selected == 'scissors') {
+      switch (housePick) {
+        case 'scissors':
+          setResult('DRAW');
+          break;
+        case 'spock':
+          setResult('LOSE');
+          handleScore(-1);
+          break;
+        case 'paper':
+          setResult('WON');
+          handleScore(1);
+          break;
+        case 'lizard':
+          setResult('WON');
+          handleScore(1);
+          break;
+        case 'rock':
+          setResult('LOSE');
+          handleScore(-1);
+          break;
+      }
+    } else if (selected == 'spock') {
+      switch (housePick) {
+        case 'scissors':
+          setResult('WON');
+          handleScore(1);
+          break;
+        case 'spock':
+          setResult('DRAW');
+          break;
+        case 'paper':
+          setResult('LOSE');
+          handleScore(-1);
+          break;
+        case 'lizard':
+          setResult('LOSE');
+          handleScore(-1);
+          break;
+        case 'rock':
+          setResult('WON');
+          handleScore(1);
+          break;
+      }
+    } else if (selected == 'paper') {
+      switch (housePick) {
+        case 'scissors':
+          setResult('LOSE');
+          handleScore(-1);
+          break;
+        case 'spock':
+          setResult('WON');
+          handleScore(1);
+          break;
+        case 'paper':
+          setResult('DRAW');
+          break;
+        case 'lizard':
+          setResult('LOSE');
+          handleScore(-1);
+          break;
+        case 'rock':
+          setResult('WON');
+          handleScore(1);
+          break;
+      }
+    } else if (selected == 'lizard') {
+      switch (housePick) {
+        case 'scissors':
+          setResult('LOSE');
+          handleScore(-1);
+          break;
+        case 'spock':
+          setResult('WON');
+          handleScore(1);
+          break;
+        case 'paper':
+          setResult('WON');
+          handleScore(1);
+          break;
+        case 'lizard':
+          setResult('DRAW');
+          break;
+        case 'rock':
+          setResult('LOSE');
+          handleScore(-1);
+          break;
+      }
+    } else if (selected == 'rock') {
+      switch (housePick) {
+        case 'scissors':
+          setResult('WON');
+          handleScore(1);
+          break;
+        case 'spock':
+          setResult('LOSE');
+          handleScore(-1);
+          break;
+        case 'paper':
+          setResult('LOSE');
+          handleScore(-1);
+          break;
+        case 'lizard':
+          setResult('WON');
+          handleScore(1);
+          break;
+        case 'rock':
+          setResult('DRAW');
+          break;
+      }
+    }
+  }
 
   function handleClick(button) {
-    setResult(true);
+    setShowResult(true);
     setSelectd(button);
+    const num = Math.floor(Math.random() * (5));
+    setTimeout(() => {
+      setHousePick(options[num]);
+      checkResult();
+    }, 500);
+  }
+
+  function handleScore(last) {
+    let temp = score;
+    temp = temp + last;
+    if (temp <= 0) {
+      temp = 0;
+    }
+    setScore(temp);
+  }
+
+  function handleTryAgain() {
+    setHousePick();
+    setResult();
+    setShowResult(false);
+    setSelectd();
   }
 
   useEffect(() => {
-    //TODO - FAZER O ALEATORIO E VERIFICAR QUEM VENCEU
-  }, [selected]);
+    checkResult();
+  }, [housePick]);
 
   return (
     <main className="game">
       <div className="game_header">
-        <div className="game_header_title">
+        <div onClick={handleTryAgain} className="game_header_title">
           <p>ROCK</p>
           <p>PAPER</p>
           <p>SCISSORS</p>
@@ -29,7 +168,7 @@ function App() {
           <div className="score">{score}</div>
         </div>
       </div>
-      <div className={`${result ? 'hide' : 'show'} game_playing`}>
+      <div className={`${showResult ? 'hide' : 'show'} game_playing`}>
         <div className="row1">
           <div onClick={() => handleClick('scissors')} className="scissors button"><img src="public\icon-scissors.svg" alt="" /></div>
         </div>
@@ -42,7 +181,23 @@ function App() {
           <div onClick={() => handleClick('rock')} className="rock button"><img src="public\icon-rock.svg" alt="" /></div>
         </div>
       </div>
-      <div className={`${result ? 'show' : 'hide'} game_result`} ></div>
+      <div className={`${showResult ? 'show' : 'hide'} game_result`} >
+        <div className="game_result_left">
+          <p>YOU PICKED</p>
+          <div className={`${selected} button`}><img src={`../public/icon-${selected}.svg`} alt="" /></div>
+        </div>
+        <div className="game_result_right">
+          <p>THE HOUSE PICKED</p>
+          {housePick ? <div className={`${housePick} button`}><img src={`../public/icon-${housePick}.svg`} alt="" /></div> : <div className="loading"></div>}
+        </div>
+        {result ? <div className="game_result_middle">
+          <p>YOU {result}</p>
+          <button onClick={handleTryAgain}>PLAY AGAIN</button>
+        </div>
+          :
+          <></>
+        }
+      </div>
       <div className="game_rules" onClick={() => setRules(true)}>Rules</div>
       <div onClick={() => setRules(false)} className={`${rules ? 'show' : 'hide'} game_popup`}>
         <div className="game_popup_content">
